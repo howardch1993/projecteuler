08bcee3dfd6eb50953afb67c4706acde9d22251f
Howard Chung <howardch1993@outlook.com>
2017-06-05 17:12:36 +0800

    update

--- p551/run_r.r
+++ p551/run_r.r
@@ -1,8 +1,4 @@
-# p551 # main func
-
 library(stringr)
-
-# 计算数字123各位数字的和,即1+2+3=6
 f1 <- function(x_num,...) {
   x_char = as.character(x_num)
   x_len = str_count(x_char)
@@ -10,14 +6,11 @@ f1 <- function(x_num,...) {
   for (i in 1:x_len) x_new[i] = as.numeric(str_sub(x_char,i,i))
   sum(x_new)
 }
-
-# 从a0计算an的数
 f2 <- function(a,...) {
   if (a == 0) b = 1
   if (a > 0) b = a + f1(a)
   b
 }
-# 计算第n位数
 f3 <- function(n = 0,an = 0,t,...) {
   a = Sys.time()
   if (t == 0) { 
@@ -37,3 +30,9 @@ f3 <- function(n = 0,an = 0,t,...) {
     return(an)
   }
 }
+options(scipen = 200)
+target = 10^15
+con <- file("D:/projecteuler/p551/output.txt",open = "wt")
+sink(con)
+log(f3(t = target))
+sink()
