8d41d7da9cea0ffc1419c46c15d7e26fa55ac4a4
Howard Chung <howardch1993@outlook.com>
2017-06-06 14:40:22 +0800

    update

--- p551/p551_main_func.r
+++ p551/p551_main_func.r
@@ -12,28 +12,18 @@ f1 <- function(x_num,...) {
 }
 
 # 从a0计算an的数
-f2 <- function(a,...) {
-  if (a == 0) b = 1
-  if (a > 0) b = a + f1(a)
-  b
-}
-# 计算第n位数
-f3 <- function(n = 0,an = 0,t,...) {
-  a = Sys.time()
-  if (t == 0) { 
-      b = Sys.time()
-      print(b - a)
-      return(f2(t))
-  }
-  if (t > 0) {
-    while (n < t) {
-      cat(n,"\t",{if(n==0) 1 else an},"\n")
-      an = f2(an)
-      n = n + 1
+f3 <- function(init_n = 1,init_num = 1,steps,...) {
+    if (init_num >= 1) {
+        f = init_num
+        while (steps > 0) {
+            cat("a",init_n," = ",f,"\n",sep = "")
+            f = f2(f)
+            init_n = init_n + 1
+            steps = steps - 1
+        }
+        cat("a",init_n," = ",f,"\n",sep = "")
+        return(f)
+    } else if (init_num < 1) {
+        cat("a0 = 1, LOL!\n")
     }
-    cat(n,"\t",an,"\n")
-    b = Sys.time()
-    print(b - a)
-    return(an)
-  }
 }
